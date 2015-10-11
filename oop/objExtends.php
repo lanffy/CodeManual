d39cbee48379e9148c48eb7fbf2e229637d1d7ad
lanffy <rao695901563@gmail.com>
2015-10-11 14:00:16 +0800

    add oop

---
+++ oop/objExtends.php
@@ -0,0 +1,23 @@
+<?php
+class foo {
+    public function printItem($string) {
+        echo 'Foo: ', $string, PHP_EOL;
+    }
+
+    public function printPHP() {
+        echo 'PHP is great.', PHP_EOL;
+    }
+}
+
+class bar extends foo {
+    public function printItem($string) {
+        echo 'Bar: ', $string, PHP_EOL;
+    }
+}
+
+$foo = new foo();
+$bar = new bar();
+$foo->printItem('baz');
+$foo->printPHP();
+$bar->printItem('baz');
+$bar->printPHP();
