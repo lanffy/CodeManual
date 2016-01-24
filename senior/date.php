e3e3282d2859f014dbfa989560bc2c4bcaf227a8
lanffy <rao695901563@gmail.com>
2016-01-24 20:46:59 +0800

    add files named date and md5

--- senior/date.php
+++ senior/date.php
@@ -1,6 +1,6 @@
 <?php
-date_default_timezone_set();
-$date1 = date("Ym");
+ini_set('date.timezone','Asia/Shanghai');
+$date1 = date("Y-m-d 10:00:00");
 echo $date1;
 
 $month_begin = date('d') == 1 ? true : false;
@@ -13,3 +13,33 @@ var_dump($date3 <= 3);
 
 $monthly = date("Y\Mm", strtotime("-10 days"));
 var_dump($monthly);
+var_dump('---');
+
+$now = time();
+$now = strtotime('2015-12-02 10:00:00');
+var_dump('now:');
+var_dump($now);
+$nine_hour_ago = $now - 9*60*60;
+var_dump('nine hour ago:');
+var_dump($nine_hour_ago);
+var_dump(intval(date('d', $nine_hour_ago)));
+
+
+$now = time();
+$today = intval(date('d', $now));
+var_dump($today);
+if ($today == 1) {
+    $change_start_time_str = date('Y-m-01 08:00:00');
+    echo 'start str:' . $change_start_time_str . PHP_EOL;
+    $change_end_time_str = date('Y-m-01 10:00:00');
+    echo 'end str:' . $change_end_time_str . PHP_EOL;
+    $change_start_time = strtotime($change_start_time_str);
+    $change_end_time = strtotime($change_end_time_str);
+    if ($now > $change_start_time && $now < $change_end_time) {
+        echo 'exchange flag' . PHP_EOL;
+    } else {
+        echo 'not exchange flag' . PHP_EOL;
+    }
+}
+
+var_dump(date('Ym', strtotime('-1 month')));
--- senior/md5.php
+++ senior/md5.php
@@ -1,7 +1,9 @@
 <?php
-$arrParams=[11668193, 'rent'];
+$arrParams=[43333, 'rent'];
 $strUnionedParam = '';
         if(!empty($arrParams)){
                         $strUnionedParam = md5(implode("&", $arrParams));
                                 }
 var_dump($strUnionedParam);
+
+var_dump(md5(43333));
