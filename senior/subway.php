ddfc6ec07aa3ff8ce5c46c938ae3a692cd769c34
raoliang <rao695901563@gmail.com>
2016-04-14 23:50:42 +0800

    add subway data

---
+++ senior/subway.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * <pre>
+ * Created by PhpStorm.
+ * User: rlanffy
+ * Date: 16/4/14
+ * Time: 下午4:21
+ * Email: liangrao@anjuke.com
+ * Desc:
+ * </pre>
+ */
+
+$city_ids = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 31, 32, 36, 39, 41, 48, 50];
+
+$new_city_ids = [25, 27, 32, 41, 48];
+
+
