1dcf9b6936ab6e4dc03a6cafbbc5e54cffa3b2f2
raoliang <rao695901563@gmail.com>
2016-09-22 23:22:55 +0800

    update

--- senior/array.php
+++ senior/array.php
@@ -5,3 +5,10 @@ var_dump($array);
 var_dump(empty($array));
 var_dump(in_array(15,$array));
 var_dump(in_array(14,$array));
+
+
+$a1 = ['a' => 'a1', 'b' => 'b1'];
+$b1 = ['c' => 'c1', 'd' => 'd1'];
+
+$c = array_merge($a1, $b1);
+var_dump($c);
