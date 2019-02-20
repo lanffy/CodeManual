77cc044ff40ad4179a9733504764bafe5e0dfd2c
lanffy <rao695901563@gmail.com>
2019-02-20 18:13:54 +0800

    c

---
+++ C/cmake-build-debug/CMakeFiles/C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C.dir/library.c.o"
+  "libC.pdb"
+  "libC.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
