18206cb3b5dce67192edb1173d80d99baf663331
lanffy <rao695901563@gmail.com>
2019-06-02 15:25:04 +0800

    add swift

---
+++ swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print(str + " aaaaa")
+
+let maxNum = 32
+
+var minNum:Int = 10
+
+print(maxNum + minNum)
+
+minNum = 20
+
+print(maxNum + minNum)
+
+
+if maxNum > minNum
+{
+    print("right")
+}
+else
+{
+    print("false")
+}
+
+var tuples = (1,2,3,4,5,"string",0.1234,name:"name")
+
+print(tuples,tuples.0,tuples.name)
+
+var a:Int
+
+a = 1
+
+var b:Int?
+
+b = nil
+
+if b == nil
+{
+    print("b value is : \(a)")
+}
