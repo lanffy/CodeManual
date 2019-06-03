480d95df189ee9dbb60b0268cba380389eb61a8b
lanffy <rao695901563@gmail.com>
2019-06-03 10:50:42 +0800

    add swift

--- swift/MyPlayground2.playground/Contents.swift
+++ swift/MyPlayground2.playground/Contents.swift
@@ -17,7 +17,7 @@ var dictionary = [
     "car":"BMW",
 ]
 print(dictionary)
-print(dictionary["car"])
+//print(dictionary["car"])
 dictionary["car"] = "Ben"
 print(dictionary)
 
@@ -47,3 +47,107 @@ case let x where x.hasSuffix("pepper"):
 default:
     print("nothing")
 }
+
+func greet(person:String, day: String?) -> String {
+    return "Hello \(person) , today is : \(day ?? "a")"
+}
+greet(person: "Lanffy", day: nil)
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), Today is : \(day)"
+}
+greet("Lanffy", on: "2019-05-27")
+
+func sortArray(scores: [Int]) -> (min: Int, max: Int, sum: Int, aver: Float) {
+    var min: Int = scores[0], max: Int = scores[0], sum: Int = 0, aver: Float = 0.0, count: Int = 0
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+        count += 1
+    }
+    aver = Float(sum) / Float(count)
+    return (min, max, sum, aver)
+}
+var result = sortArray(scores: [1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1,10,1])
+print(result)
+
+func funcNest() -> Int {
+    var y = 10
+    func add(plus: Int) {
+        y += plus
+    }
+    add(plus: 20)
+    return y
+}
+print(funcNest())
+
+func funcReturnFunc(plus: Int) -> ((Int) -> Int) {
+    func sum(number: Int) -> Int {
+        return number + plus
+    }
+    return sum
+}
+var increment = funcReturnFunc(plus: 10)
+print(increment(10))
+var numbers = [1,2,3]
+var number2 = numbers.map({ (number: Int) -> Int in
+    if number % 2 == 0 {
+        return 0
+    }
+    return number * 2
+})
+print(number2)
+var number3 = numbers.map({ number in 2 * number })
+print(number3)
+var sortNumber = numbers.sorted{$0 > $1}
+print(sortNumber)
+
+class Shape {
+    var numberOfSide = 0
+    var name = ""
+    
+    init(numberOfSide: Int, name: String) {
+        self.numberOfSide = numberOfSide
+        self.name = name
+    }
+    
+    func toString() -> String {
+        return "A Shape with \(numberOfSide) sides,It's name : \(name)"
+    }
+}
+var shape = Shape(numberOfSide: 4, name: "Square")
+//shape.numberOfSide = 33
+print(shape.toString())
+
+class Square: Shape {
+    var sideLength: Double
+    var perimeter: Double
+    {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3
+        }
+    }
+    
+    init(sideLength: Double, numberOfSide: Int, name: String) {
+        self.sideLength = sideLength
+        super.init(numberOfSide: numberOfSide, name: name)
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func toString() -> String {
+        return "A Shape with \(numberOfSide) sides,It's name : \(name), Area:\(self.area())"
+    }
+}
+var square = Square(sideLength: 1.1, numberOfSide: 4, name: "square 1")
+print(square.toString())
+print(square.perimeter)
