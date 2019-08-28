cd2b5fee4a5340ea67f3979d55b574bec667de78
lanffy <rao695901563@gmail.com>
2019-08-28 10:33:55 +0800

    update

--- swift/MyPlayground_office.playground/Contents.swift
+++ swift/MyPlayground_office.playground/Contents.swift
@@ -594,3 +594,23 @@ for item in library {
 }
 print(movieCount, songCount)
 
+extension Double {
+    var km: Double {
+        return self * 1000.0
+    }
+    var m: Double {
+        return self
+    }
+    var cm: Double {
+        return self / 100.0
+    }
+    var mm: Double {
+        return self / 1000.0
+    }
+    var ft: Double {
+        return self / 3.28084
+    }
+}
+
+let oneTnch = 25.4.mm
+print(oneTnch)
